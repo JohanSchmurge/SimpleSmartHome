3ef6a8d322c7c34c743fd4919dd6b6da48b510cf
Johan Schmurge <57623145+JohanSchmurge@users.noreply.github.com>
2021-12-29 16:45:48 +0300

    add default_config

---
+++ default_config.py
@@ -0,0 +1,14 @@
+#SSID, password for Wi-Fi station interface.
+SSID = ('ssid', 'password')
+
+
+# Default SSID and password for AP interface.
+AP_SSID = ('ap_name', 'ap_password')
+
+# Enable or disable Access point and station interfaces.
+AP = True
+STA = True
+
+# Time zone for sync time.
+TZ = 3
+NTP_SERVER = 'pool.ntp.org'
