6d9763c061e172642293932af7e0b788a3bba0c9
Johan Schmurge <57623145+JohanSchmurge@users.noreply.github.com>
2021-12-29 16:42:58 +0300

    Delete default_config.py

--- default_config.py
+++
@@ -1,9 +0,0 @@
-# SSID, password for Wi-Fi station interface.
-SSID = ('esid', 'password')
-
-# Default SSID and password for AP interface.
-AP_SSID = ('MicroPython', 'micropythoN')
-
-# Enable or disable Access point and station interfaces.
-AP = True
-STA = True
